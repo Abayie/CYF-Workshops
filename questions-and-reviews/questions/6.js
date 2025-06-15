2e3a5e8b5624325e88eed4d3f80789afe8aa0023
3bdullah-saleh <abdullahedu0@gmail.com>
2025-06-15 10:36:50 +0100

    fix: correct loop logic in printTimesTables by declaring i and incrementing it

--- questions-and-reviews/questions/6.js
+++ questions-and-reviews/questions/6.js
@@ -3,11 +3,13 @@
 
 
 function printTimesTables(n) {
-    const count = 1;
+    let count = 1;
+    let i = 1;
     while(count <= 12) {
         const product = n * i;
         console.log(`${n} * ${i} = ${product}`);
         count++;
+        i++
     }
 }
-
+printTimesTables(1)
